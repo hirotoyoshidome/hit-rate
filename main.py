b9d784c0c133760f03dd153d54e85f5595600960
Hiroto Yoshidome <h.yoshidome@diamondhead.tech>
2019-09-13 17:29:39 +0900

    fix indent and output non cache resources

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+# execute command
 # python3 main.py https://google.com/
-# TODO
-# 算出方法精査(現段階では7割くらいのキャッシュ率)⇒おそらく画像以外のパスもトータルに入っているため、低い
 
 # consts
 JUDGEMENT = 'hit'
@@ -34,11 +33,11 @@ driver = webdriver.Chrome(desired_capabilities=caps) # , chrome_options=opt
 driver.get(url)
 
 for entry in driver.get_log('performance'):
-    str_req_info = entry.get('message')
-    dict_req_info = json.loads(str_req_info)
-    response_info = dict_req_info.get('message').get('params').get('response')
-    if None != response_info:
-        contents_url.append(response_info.get('url'))
+  str_req_info = entry.get('message')
+  dict_req_info = json.loads(str_req_info)
+  response_info = dict_req_info.get('message').get('params').get('response')
+  if None != response_info:
+    contents_url.append(response_info.get('url'))
 driver.quit()
 
 # # verify
@@ -46,23 +45,28 @@ driver.quit()
 # # python3 main.py https:google.com > result.txt
 # # で対象のリソースの確認ができます
 # for x in contents_url:
-#     print(x)
+#   print(x)
 
 total_count = len(contents_url)
 hit_count = 0
+non_cached_resources = []
 
 for content in contents_url:
-    info = requests.get(content)
-    val = ""
-    if HEADER_TARGET_ITEM in info.headers:
-        val = info.headers[HEADER_TARGET_ITEM]
+  info = requests.get(content)
+  val = ""
+  if HEADER_TARGET_ITEM in info.headers:
+    val = info.headers[HEADER_TARGET_ITEM]
 
-    if JUDGEMENT in val.lower():
-        hit_count = hit_count + 1
+  if JUDGEMENT in val.lower():
+    hit_count = hit_count + 1
+  else:
+    non_cached_resources.append(content)
 
 hit_percentage = (hit_count / total_count * 100)
 
 # output
 print('total hit count : ' + str(total_count))
 print('hit percentage  : ' + str(hit_percentage))
-
+print('###non cached resources###')
+for c in non_cached_resources:
+  print(c)
