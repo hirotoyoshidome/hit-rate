c3891a9e740b171f6c190162c7c9bd06a0f0b9ae
Hiroto Yoshidome <h.yoshidome@diamondhead.tech>
2019-09-17 11:22:46 +0900

    fix docker file

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,18 @@ FROM python:3.6.9-alpine3.9
 WORKDIR /usr/src
 
 RUN apk update && apk upgrade
-RUN apk add --update chromium
-RUN apk add --update chromium-chromedriver
+RUN apk add --update \
+        chromium \
+        udev \
+        chromium-chromedriver \
+        openssl
 
-RUN pip install requests
-RUN pip install selenium
+RUN pip install \
+        requests \
+        selenium
 # RUN pip install chromedriver-binary
+
+RUN cp /usr/lib/chromium/chromedriver /usr/src/
 
 COPY . .
 
--- main.py
+++ main.py
@@ -23,30 +23,37 @@ def check_exteensions(content):
       return False
   return True
 
+# set env
 current_dir = str(subprocess.check_output(['pwd'])).replace('b\'', '').replace('\\n\'', '')
 os.environ['PATH'] = os.environ.get('PATH') + ':' + current_dir
 
 caps = DesiredCapabilities.CHROME
 caps['loggingPrefs'] = {'performance': 'ALL'}
 
+opt = selenium.webdriver.ChromeOptions()
 ## comment out because it did not work without opening a browser
-# opt = selenium.webdriver.ChromeOptions()
 # opt.add_argument('--blink-settings=imagesEnabled=false')
+opt.add_argument('--headless')
+opt.add_argument('--no-sandbox')
+opt.add_argument('--disable-dev-shm-usage')
 
 url = sys.argv[1]
-
 contents_url = []
 
-driver = webdriver.Chrome(desired_capabilities=caps) # , chrome_options=opt
-driver.get(url)
+# run selenium with chrome
+try:
+  driver = webdriver.Chrome(desired_capabilities=caps, chrome_options=opt)
+  driver.get(url)
 
-for entry in driver.get_log('performance'):
-  str_req_info = entry.get('message')
-  dict_req_info = json.loads(str_req_info)
-  response_info = dict_req_info.get('message').get('params').get('response')
-  if None != response_info:
-    contents_url.append(response_info.get('url'))
-driver.quit()
+  for entry in driver.get_log('performance'):
+    str_req_info = entry.get('message')
+    dict_req_info = json.loads(str_req_info)
+    response_info = dict_req_info.get('message').get('params').get('response')
+    if None != response_info:
+      contents_url.append(response_info.get('url'))
+  driver.quit()
+except:
+  raise Exception('Chrome Driver ERROR.')
 
 ## verify
 ## if you want to check all static resources, please add this comments .
@@ -58,9 +65,15 @@ total_count = len(contents_url)
 hit_count = 0
 exclusion_count = 0
 non_cached_resources = []
+error_count = 0
 
+# run requests with selenium result
 for content in contents_url:
-  info = requests.get(content)
+  try:
+    info = requests.get(content)
+  except:
+    error_count = error_count + 1
+    continue
   val = ""
   if HEADER_TARGET_ITEM in info.headers:
     val = info.headers[HEADER_TARGET_ITEM]
@@ -73,11 +86,15 @@ for content in contents_url:
     non_cached_resources.append(content)
 
 # post processing
-hit_percentage = (hit_count / (total_count - exclusion_count) * 100)
+try:
+  hit_percentage = (hit_count / (total_count - exclusion_count) * 100)
+except ZeroDivisionError:
+  raise Exception('Could not divide by 0.')  
 
 # output
 print('total hit count : ' + str(total_count - exclusion_count))
 print('hit percentage  : ' + str(hit_percentage))
+print('error count : ' + str(error_count))
 
 ## if you want to check non cached resources, please add this comments .
 # print('###non cached resources###')
